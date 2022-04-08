0d17f87f2c0b083f2bad63aa527ad2e384bf5ea8
holmberd <holmberd@gmail.com>
2022-04-08 07:07:28 -0700

    Remove unused functions

--- src/utils.js
+++ src/utils.js
@@ -1,46 +1,16 @@
-export function throttle(fn, wait) {
-  var time = Date.now();
-  return function () {
-    if ((time + wait - Date.now()) < 0) {
-      fn();
-      time = Date.now();
-    }
-  }
-}
-
 export function debounce(callback, delay, leading = true) {
-  let start = now();
+  let start = Date.now();
 
   return (...args) => {
     if (leading) {
       leading = false;
       callback(...args);
     }
-    if (now() - start >= delay) {
+    if (Date.now() - start >= delay) {
       callback(...args);
     } else {
-      start = now();
+      start = Date.now();
     }
   }
 }
 
-/*
-
-function throttle(fn, wait) {
-  let lastFn, lastRan;
-  return (...args) => {
-    if (!lastRan) {
-      fn(...args);
-      lastRan = Date.now();
-    } else {
-      clearTimeout(lastFunc);
-      lastFunc = setTimeout(() => {
-        if ((Date.now() - lastRan) >= wait) {
-          fn(...args);
-          lastRan = Date.now();
-        }
-      }, wait - (Date.now() - lastRan));
-    }
-  }
-}
-*/
